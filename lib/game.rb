6ad789ae792b91dc3aa06c9794406875e9d890a3
Atul Vishwakarma <atulvishw240@gmail.com>
2024-10-25 07:47:17 +0530

    Create outline of game

---
+++ lib/game.rb
@@ -0,0 +1,4 @@
+# 1. Computer will choose a word
+# 2. Player gets 5 guesses to guess the word
+# 3. If player guesses the word correctly >= 5 tries THEN he wins
+# 4. If out of tries THEN player loses

b2f95b6c4893cb98026e204a0def3d7a5e03ffe7
Atul Vishwakarma <atulvishw240@gmail.com>
2024-10-25 10:51:05 +0530

    Implement select word functionality

---
+++ hangman.rb
@@ -0,0 +1,5 @@
+require_relative 'lib/game'
+
+game = Game.new
+
+puts game.choose_word
--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,9 @@
 # 2. Player gets 5 guesses to guess the word
 # 3. If player guesses the word correctly >= 5 tries THEN he wins
 # 4. If out of tries THEN player loses
-class Gem
+class Game
+  MAX_TRIES = 5
+  TOTAL_WORDS = 9894
   attr_accessor :secret_word
 
   def initialize
@@ -14,7 +16,7 @@ class Gem
   end
 
   # ALL PRIVATE METHODS ARE BELOW
-  private
+  # private
 
   def display
     # Display the state of board
@@ -22,6 +24,30 @@ class Gem
 
   def choose_word
     # Choose the secret word from dictionary.txt
+    rndm_index = random_index_generation
+    fname = 'dictionary.txt'
+    if file_exist?(fname)
+      array = File.readlines(fname)
+      word = array[rndm_index].chomp
+
+      if word.length.between?(5, 12)
+        return word
+      else
+        choose_word
+      end
+
+    else
+      "File 'dictionary.txt' doesn't exist"
+    end
+  end
+
+  def file_exist?(fname)
+    File.exist?(fname)
+  end
+
+  def random_index_generation
+    random = Random.new
+    random.rand(0..TOTAL_WORDS - 1)
   end
 
   def guess_word

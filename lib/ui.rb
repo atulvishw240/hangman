5ac8f224cc521796aa834d9092fbbe5dcadfd43a
atulvishw240 <atulvishw240@gmail.com>
2025-08-04 17:41:59 +0530

    Display game state

--- lib/ui.rb
+++ lib/ui.rb
@@ -4,4 +4,10 @@ class UI
   def initialize(game)
     @game = game
   end
+
+  def display
+    puts game.input_fields.join(' ')
+    puts "Wrong guesses: #{game.wrong_guesses.join(', ')}"
+    puts "You have #{game.lives} guesses left"
+  end
 end

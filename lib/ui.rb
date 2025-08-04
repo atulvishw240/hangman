a88d541255c2a7ffad062716fbb53eb0cce6ae63
atulvishw240 <atulvishw240@gmail.com>
2025-08-04 17:47:06 +0530

    Take player guess

--- lib/ui.rb
+++ lib/ui.rb
@@ -10,4 +10,20 @@ class UI
     puts "Wrong guesses: #{game.wrong_guesses.join(', ')}"
     puts "You have #{game.lives} guesses left"
   end
+
+  def make_guess
+    print "Enter your guess: "
+    guess = gets.chomp
+
+    return guess if valid?(guess)
+
+    raise "You have made an incorrect guess, Try again!!"
+    
+    make_guess
+  end
+
+  def valid?(guess)
+    /[a-z]/.match?(guess) && guess.length == 1 && !game.input_fields.include?(guess) &&
+    !game.wrong_guesses.include?(guess)
+  end
 end

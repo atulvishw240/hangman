b51b11f9aa5b4f9a99c2680f172b5f8d8f9b78fb
atulvishw240 <atulvishw240@gmail.com>
2025-08-04 17:32:56 +0530

    Change #update

--- lib/game_engine.rb
+++ lib/game_engine.rb
@@ -1,22 +1,28 @@
 # Contains Game Logic
 class GameEngine
-  attr_accessor :word, :correct_guesses, :wrong_guesses
+  attr_accessor :word, :input_fields, :wrong_guesses
 
-  def initialize(word, correct_guesses, wrong_guesses)
+  def initialize(word, input_fields, wrong_guesses)
     @word = word
-    @correct_guesses = correct_guesses
+    @input_fields = input_fields
     @wrong_guesses = wrong_guesses
   end
 
   def update(letter)
     if word.include?(letter)
-      correct_guesses << letter
+      update_input_fields(letter)
     else
       wrong_guesses << letter
     end
   end
 
+  def update_input_fields(letter)
+    word.chars.each_with_index do |char, index|
+      input_fields[index] = letter if char == letter
+    end
+  end
+
   def won?
-    word.chars.uniq.size == correct_guesses.uniq.size
+    word.chars == input_fields
   end
 end

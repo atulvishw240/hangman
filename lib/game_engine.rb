1d5e371226d7f243b09bfdfe49dc4685ad2a9c67
atulvishw240 <atulvishw240@gmail.com>
2025-08-04 16:26:23 +0530

    Update game state

--- lib/game_engine.rb
+++ lib/game_engine.rb
@@ -7,4 +7,12 @@ class GameEngine
     @correct_guesses = correct_guesses
     @wrong_guesses = wrong_guesses
   end
+
+  def update(letter)
+    if word.include?(letter)
+      correct_guesses << letter
+    else
+      wrong_guesses << letter
+    end
+  end
 end
